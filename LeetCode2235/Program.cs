47502d358ac5a62c06285ab82a545ef38dd7fffb
LINCHENGCHI <d11416112@gs.takming.edu.tw>
2025-10-08 11:54:56 +0800

    Add files via upload

---
+++ LeetCode2235/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode2235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            input = Console.ReadLine();
+            int r = Convert.ToInt32(input);
+
+            string input1;
+            input1 = Console.ReadLine();
+            int k = Convert.ToInt32(input1);
+            Console.WriteLine(Sum(r, k));
+
+            int Sum(int num1, int num2)
+            {
+                //分析函式
+                //return(num1 + num2);
+                //1.輸入
+
+                //2.處理
+                int num;
+                num = num1 + num2;
+                //3.輸出
+                int a;
+                a = num;
+                return a;
+            }
+        }
+    }
+}
